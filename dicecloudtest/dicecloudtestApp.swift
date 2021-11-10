55eddfb1376322e10e27ed9ca892c88158a1ba44
tuckerhoppe <tuckerhoppe22@gmail.com>
2021-11-10 15:04:01 -0700

    Initial Commit

---
+++ dicecloudtest/dicecloudtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicecloudtestApp.swift
+//  dicecloudtest
+//
+//  Created by Tucker on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct dicecloudtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
